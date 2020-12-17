da810ef6a3eee9d709b9135b534db28f7480762c
Lemon <516330797@qq.com>
2020-12-17 11:02:26 +0800

    引入全ç全局组件<Nav/>

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>Money.vue</div>
+  <div>
+    Money.vue
+    <Nav/>
+  </div>
 </template>
 
 <script lang="ts">
 export default {
-  name: 'Money'
+  name: 'Money',
 };
 </script>
 
