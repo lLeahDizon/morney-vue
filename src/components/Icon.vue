3441d86def8e05069c23eeb67a3edfb8db42f0f1
Lemon <516330797@qq.com>
2020-12-18 07:16:27 +0800

    创建<Icon />

---
+++ src/components/Icon.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg class="icon">
+    <use :xlink:href="'#'+name"/>
+  </svg>
+</template>
+
+<script lang="ts">
+
+// 引入所有的 svg
+const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {importAll(require.context('../assets/icons/', true, /\.svg$/));} catch (error) {console.log(error);}
+
+export default {
+  props: ['name'],
+  name: 'Icon'
+};
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  /*宽高一个字大小 参考 Iconfont 文档*/
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="nav">
     <router-link to="/money">
+      <Icon name="money"/>
       记账
     </router-link>
     |
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/labels">
+      <Icon name="label"/>
+      标签
+    </router-link>
     |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/statistics">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
-
-// 引入所有的 svg
-const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext)
-try {importAll(require.context('../assets/icons/', true, /\.svg$/))} catch (error) {console.log(error)}
-
 export default {
-  name: 'Nav'
-}
+  name: 'Nav',
+};
 </script>
 
 <style lang="scss" scoped> /*scoped只会影响当前template的样式*/
