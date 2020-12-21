453e711d2434abfb5c89e5a2b4a10b021e24068c
Lemon <516330797@qq.com>
2020-12-21 15:04:58 +0800

    完成 Notes 组件

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -1,34 +1,48 @@
 <template>
   <label class="notes">
     <span class="name">备注</span>
-    <input type="text" placeholder="在这里添加备注">
+    <input
+      type="text"
+      :value="value"
+      @input="onChange"
+      placeholder="在这里添加备注"
+    >
   </label>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'Notes'
-};
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Notes extends Vue {
+    value = '';
+
+    onChange(event: KeyboardEvent) {
+      const input = event.target as HTMLInputElement;
+      this.value = input.value;
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
-.notes {
-  font-size: 14px;
-  background: #f5f5f5;
-  padding-left: 16px;
-  display: flex;
-  align-items: center;
+  .notes {
+    font-size: 14px;
+    background: #f5f5f5;
+    padding-left: 16px;
+    display: flex;
+    align-items: center;
 
-  .name {
-    padding-right: 16px;
-  }
+    .name {
+      padding-right: 16px;
+    }
 
-  input {
-    height: 64px;
-    flex-grow: 1;
-    background: transparent;
-    border: none;
-    padding-right: 16px;
+    input {
+      height: 64px;
+      flex-grow: 1;
+      background: transparent;
+      border: none;
+      padding-right: 16px;
+    }
   }
-}
 </style>
