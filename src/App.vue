a033a8220f8f50c265a9c4237f6a7307e361f7bd
Lemon <516330797@qq.com>
2020-12-23 10:02:18 +0800

    完成 Labels.vue 样式

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,16 @@
 </template>
 
 <style lang="scss">
-@import "~@/assets/style/helper.scss";
-@import "~@/assets/style/reset.scss";
+  @import "~@/assets/style/helper.scss";
+  @import "~@/assets/style/reset.scss";
 
-body {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #333;
-  line-height: 1.5;
-  font-family: $font-hei;
-  font-size: 16px;
-}
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #333;
+    line-height: 1.5;
+    font-family: $font-hei;
+    font-size: 16px;
+    background: #f5f5f5;
+  }
 </style>
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,11 +1,65 @@
 <template>
   <Layout>
-    <p>Labels.vue</p>
+    <ol class="tags">
+      <li><span>衣</span>
+        <Icon name="right"/>
+      </li>
+      <li><span>食</span>
+        <Icon name="right"/>
+      </li>
+      <li><span>住</span>
+        <Icon name="right"/>
+      </li>
+      <li><span>行</span>
+        <Icon name="right"/>
+      </li>
+    </ol>
+    <div class="createTag-wrapper">
+      <button class="createTag">新建标签</button>
+    </div>
   </Layout>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'Labels',
-};
+  export default {
+    name: 'Labels',
+  };
 </script>
+
+<style lang="scss" scoped>
+  .tags {
+    background: white;
+    font-size: 16px;
+    padding-left: 16px;
+
+    > li {
+      min-height: 44px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      border-bottom: 1px solid #e6e6e6;
+
+      svg {
+        width: 18px;
+        height: 18px;
+        color: #666;
+        margin-right: 16px;
+      }
+    }
+  }
+
+  .createTag {
+    background: #767676;
+    color: white;
+    border-right: 4px;
+    border: none;
+    height: 40px;
+    padding: 0 16px;
+
+    &-wrapper {
+      text-align: center;
+      padding: 16px;
+      margin-top: 44-16px;
+    }
+  }
+</style>
