8031d4d7f8a0b3a9adda41cdc6d4e6682b316156
Lemon <516330797@qq.com>
2020-12-27 07:33:35 +0800

    封装 window.createTag()

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -29,19 +29,14 @@
     createTag() {
       const name = window.prompt('请输入标签名');
       if (name) {
-        const message = tagListModel.create(name);
-        if (message === 'duplicated') {
-          window.alert('标签名重复了');
-        } else if (message === 'success') {
-          window.alert('添加成功');
-        }
+        window.createTag(name);
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
-  .tags-wrapper{
+  .tags-wrapper {
     display: flex;
     flex-direction: column;
     height: 100%;
