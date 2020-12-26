45b7ec186fdcd2a92181ea6a94036ac0e0a13d19
Lemon <516330797@qq.com>
2020-12-27 07:24:12 +0800

    修改 Labels 样式，兼容长列表的样式

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,13 +1,15 @@
 <template>
   <Layout>
-    <div class="tags">
-      <router-link class="tag" v-for="tag in tags" :key="tag.id" :to="`/labels/edit/${tag.id}`">
-        <span>{{ tag.name }}</span>
-        <Icon name="right"/>
-      </router-link>
-    </div>
-    <div class="createTag-wrapper">
-      <Button class="createTag" @click="createTag">新建标签</Button>
+    <div class="tags-wrapper">
+      <div class="tags">
+        <router-link class="tag" v-for="tag in tags" :key="tag.id" :to="`/labels/edit/${tag.id}`">
+          <span>{{ tag.name }}</span>
+          <Icon name="right"/>
+        </router-link>
+      </div>
+      <div class="createTag-wrapper">
+        <Button class="createTag" @click="createTag">新建标签</Button>
+      </div>
     </div>
   </Layout>
 </template>
@@ -39,39 +41,47 @@
 </script>
 
 <style lang="scss" scoped>
-  .tags {
-    background: white;
-    font-size: 16px;
-    padding-left: 16px;
+  .tags-wrapper{
+    display: flex;
+    flex-direction: column;
+    height: 100%;
 
-    > .tag {
-      min-height: 44px;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      border-bottom: 1px solid #e6e6e6;
+    > .tags {
+      font-size: 16px;
+      padding-left: 16px;
+      overflow: auto;
+      flex-grow: 1;
 
-      svg {
-        width: 18px;
-        height: 18px;
-        color: #666;
-        margin-right: 16px;
+      > .tag {
+        background: white;
+        min-height: 44px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        border-bottom: 1px solid #e6e6e6;
+
+        svg {
+          width: 18px;
+          height: 18px;
+          color: #666;
+          margin-right: 16px;
+        }
       }
     }
-  }
 
-  .createTag {
-    background: #767676;
-    color: white;
-    border-right: 4px;
-    border: none;
-    height: 40px;
-    padding: 0 16px;
+    > .createTag {
+      background: #767676;
+      color: white;
+      border-right: 4px;
+      border: none;
+      height: 40px;
+      padding: 0 16px;
 
-    &-wrapper {
-      text-align: center;
-      padding: 16px;
-      margin-top: 44-16px;
+      &-wrapper {
+        text-align: center;
+        padding: 16px;
+        margin-top: 28-16px;
+      }
     }
   }
 </style>
